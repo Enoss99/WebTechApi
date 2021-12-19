eb078813497880f19ce3b965c5bc0e332d64101c
SamaeLight <24samael@gmail.com>
2021-12-19 21:15:36 +0100

    Update Pannier.cs

--- src/Pannier.cs
+++ src/Pannier.cs
@@ -1,1 +1,31 @@
+namespace courses
+{
+    public class Pannier
+    {
+        private Produit _produit;
+        private int quantite;
 
+        public int Quantite
+        {
+            set { quantite = value; }
+            get { return quantite; }
+        }
+
+        public Produit Produit
+        {
+            set { _produit = value; }
+            get { return _produit; }
+        }
+
+        public Pannier()
+        {
+
+        }
+
+        public Pannier(Produit produit, int quantite)
+        {
+            _produit = produit;
+            this.quantite = quantite;
+        }
+    }
+}

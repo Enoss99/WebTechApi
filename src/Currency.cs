726b5317aa29ddd412edc69778f063114ca326bf
SamaeLight <24samael@gmail.com>
2021-12-19 17:54:07 +0100

    Produit
    Création produit

--- src/Currency.cs
+++ src/Currency.cs
@@ -1,1 +1,30 @@
+using System;
 
+namespace courses
+{
+    public class Produit : IProduit
+    {
+
+        public String Nom { get; }
+        public String Description { get; }
+
+        public Price Prix { get; }
+
+        public Produit()
+        {
+        }
+
+        public Produit(string nom, string description, Price prix)
+        {
+            Nom = nom;
+            Description = description;
+            Prix = prix;
+        }
+
+        public override string ToString()
+        {
+            return "\n" + "nom : " + Nom + "\n" + "description : " + Description + "\n"
+                   + "prix : " + Prix + "\n";
+        }
+    }
+}
